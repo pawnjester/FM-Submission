cf32258549255b9cc0552fae94b42380bb508a44
pawnjester <ebukeokonji@gmail.com>
2021-01-26 23:34:25 +0100

    - clean up

--- ui/src/test/java/com/example/ui/core/MainCoroutinesRule.kt
+++ ui/src/test/java/com/example/ui/core/MainCoroutinesRule.kt
@@ -7,10 +7,8 @@ import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import org.junit.rules.TestRule
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import org.junit.runners.model.Statement
 
 @ExperimentalCoroutinesApi
 class MainCoroutineRule(
